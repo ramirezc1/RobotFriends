f748dec07a0219f8a39d4b8fc05475e3b65a3ec4
Cristian Ramirez <ramirezcristian81@live.com>
2020-10-08 14:05:35 -0700

    Updates

---
+++ precache-manifest.8ae2d33b1842633e14e6303e87020689.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4dbf6d8d97f8feca418a4ff882cedc9",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "5911b39973ce2e517182",
+    "url": "/RobotFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "98091c23a2e1f73e86d5",
+    "url": "/RobotFriends/static/css/main.83789876.chunk.css"
+  },
+  {
+    "revision": "5911b39973ce2e517182",
+    "url": "/RobotFriends/static/js/2.7b75e7c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RobotFriends/static/js/2.7b75e7c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98091c23a2e1f73e86d5",
+    "url": "/RobotFriends/static/js/main.51086cf4.chunk.js"
+  },
+  {
+    "revision": "b9d1827298eddeb02b9f",
+    "url": "/RobotFriends/static/js/runtime-main.42d37932.js"
+  }
+]);
